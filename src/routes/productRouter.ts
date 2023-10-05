48b1c10e0c886b966d432a55dc4bef94d4adb86d
Lucas52352 <lucasrius86@gmail.com>
2023-10-05 12:29:24 -0300

    update product

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -3,13 +3,14 @@ import { getOneProductHandler } from '../handlers/products/getOneProductHandler'
 import { getAllProductsHandler } from '../handlers/products/getAllProductsHandler';
 import { postProductHandler } from '../handlers/products/postProductHandler';
 import { deleteProductHanlder } from '../handlers/products/deleteProductHandler';
+import { updateProductHandler } from '../handlers/products/updateProductHandler';
 
 const productRouter = Router();
 
 productRouter.get('/products', getAllProductsHandler)
 productRouter.get('/product/:_id', getOneProductHandler)
 productRouter.post('/product', postProductHandler)
-// productRouter.put('/product/:id', updateProduct)
+productRouter.put('/product/:_id', updateProductHandler)
 productRouter.delete('/product/:id', deleteProductHanlder)
 
 export default productRouter;
